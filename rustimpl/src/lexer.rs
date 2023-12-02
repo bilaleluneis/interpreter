4e45708c41cd9bfa456d23853241396161c1c54a
bilaleluneis <bilaleluneis@gmail.com>
2023-12-02 09:46:23 -0700

    rust lexer impl can now recognize minimum language construct.

--- rustimpl/src/lexer.rs
+++ rustimpl/src/lexer.rs
@@ -21,19 +21,66 @@ impl Lexer {
     }
 
     pub fn next_token(&mut self) -> Token {
-        //TODO need to handle white space
+        while self.peek().is_ascii_whitespace() {
+            self.advance()
+        }
+
+        match self.next_char().as_str() {
+            ch if ch.is_empty() => Token::Eof,
+            ch if ch.chars().all(char::is_alphabetic) => self.literal_token(),
+            ch if ch.chars().all(char::is_numeric) => self.numeric_token(),
+            ch => Token::lookup(ch),
+        }
+    }
+
+    fn literal_token(&mut self) -> Token {
+        let mut ch = self.byte_to_str();
+        while !self.peek().is_ascii_whitespace() && self.peek().is_ascii_alphabetic() {
+            ch += self.next_char().as_str();
+        }
+        let tok = Token::lookup(ch.as_str());
+        match tok {
+            Token::Illegal(t) => Token::Ident(t),
+            _ => tok
+        }
+    }
+
+    fn numeric_token(&mut self) -> Token {
+        let mut ch = self.byte_to_str();
+        while !self.peek().is_ascii_whitespace() && self.peek().is_ascii_digit() {
+            ch += self.next_char().as_str();
+        }
+        Token::lookup(ch.as_str()) // shoult return Int or Illigal token
+    }
+
+    // TODO:can you return char instead of string as utf8?
+    fn next_char(&mut self) -> String {
+        self.ch_under_inspection = self.peek();
+        self.advance();
+        self.byte_to_str()
+    }
+
+    fn peek(&self) -> u8 {
         let pos = self.next_read_position;
-        let next_ch = if pos >= self.input.len() {
-            self.ch_under_inspection = 0u8;
-            "".to_string()
+        let ch = if pos >= self.input.len() {
+            0u8
         } else {
-            let ch = str::from_utf8(&[self.input.as_bytes()[pos]]).unwrap_or("").to_string();
-            self.ch_under_inspection = ch.as_bytes()[0];
-            ch
+            self.input.as_bytes()[pos]
         };
+        ch
+    }
+
+    //TODO: come up with better name
+    fn byte_to_str(&self) -> String {
+        match self.ch_under_inspection {
+            0u8 => "".to_string(),
+            b => str::from_utf8(&[b]).unwrap_or("").to_string()
+        }
+    }
+
+    fn advance(&mut self) {
         self.prev_read_position = self.next_read_position;
         self.next_read_position += 1;
-        Token::lookup(next_ch.as_str())
     }
 }
 
@@ -55,6 +102,52 @@ mod tests {
             Token::Lbrace,
             Token::Rbrace,
             Token::Eof,
+        ];
+        for expected_tok in expected_tokens {
+            assert_eq!(lexer.next_token(), expected_tok);
+        }
+    }
+
+    #[test]
+    fn min_lang_construct() {
+        let mut lexer = Lexer::new(r#"
+            let five = 5;
+            let ten = 10;
+            let add = fn(x, y) { x + y};
+            let result = add(five, ten);
+        "#.to_string());
+
+        let expected_tokens = [
+            // let five = 5;
+            Token::Let,
+            Token::Ident("five".to_string()),
+            Token::Assign,
+            Token::Int(5),
+            Token::Semicolon,
+
+            //let ten = 10;
+            Token::Let,
+            Token::Ident("ten".to_string()),
+            Token::Assign,
+            Token::Int(10),
+            Token::Semicolon,
+
+            //let add = fn(x, y) {x, y};
+            Token::Let,
+            Token::Ident("add".to_string()),
+            Token::Assign,
+            Token::Function,
+            Token::Lparen,
+            Token::Ident("x".to_string()),
+            Token::Comma,
+            Token::Ident("y".to_string()),
+            Token::Rparen,
+            Token::Lbrace,
+            Token::Ident("x".to_string()),
+            Token::Plus,
+            Token::Ident("y".to_string()),
+            Token::Rbrace,
+            Token::Semicolon,
         ];
         for expected_tok in expected_tokens {
             assert_eq!(lexer.next_token(), expected_tok);

0357b11ad191739d1fe64c7bd8f6a628dd0dac49
bilaleluneis <bilaleluneis@gmail.com>
2024-06-29 07:51:46 -0700

    add integer type to node

---
+++ goimpl/ast/integer.go
@@ -0,0 +1,14 @@
+package ast
+
+import "goimpl/token"
+
+type IntegerLiteral struct {
+	Tok   token.Token // the token.INT token
+	Value int64
+}
+
+func (IntegerLiteral) expressionNode() {}
+
+func (il IntegerLiteral) TokenLiteral() string { return il.Tok.Literal }
+
+func (il IntegerLiteral) String() string { return il.Tok.Literal }

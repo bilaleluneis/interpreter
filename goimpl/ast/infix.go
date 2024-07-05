42ee84f51ca5f5ed8ad0d4341d6990e07169ca0f
bilaleluneis <bilaleluneis@gmail.com>
2024-07-04 19:13:08 -0700

    added infix expression node ex: 5 + 5;

---
+++ goimpl/ast/infix.go
@@ -0,0 +1,19 @@
+package ast
+
+import "goimpl/token"
+
+// InfixExpression <InfixExpression> ::= <expression> <infix operator> <expression>
+type InfixExpression struct {
+	Left     Expression
+	Tok      token.Token // The operator token, e.g. "+"
+	Operator string
+	Right    Expression
+}
+
+func (InfixExpression) expressionNode() {}
+
+func (ie InfixExpression) TokenLiteral() string { return ie.Tok.Literal }
+
+func (ie InfixExpression) String() string {
+	return "(" + ie.Left.String() + " " + ie.Operator + " " + ie.Right.String() + ")"
+}
